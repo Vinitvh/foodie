9c14c1634722d3dc5f0ef360ece7daac27340036
Vinitvh <vineet.vh@gmail.com>
2025-07-07 00:48:49 +0530

    Add TailwindCss and Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <nav className="bg-emerald-900">
+      <div className="flex justify-between py-4">
+        <div className="ml-12">
+          <span className="text-3xl text-white font-bold">Foodie</span>
+        </div>
+        <ul className="flex px-12 text-white font-bold">
+          <li>
+            <a href="#" className="mx-8">
+              Sign Up
+            </a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
